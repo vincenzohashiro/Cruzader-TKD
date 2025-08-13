e843a8f83b20d2660d22a4fcd0e93f54b647c945
vince <vincentmangalili67@gmail.com>
2025-08-13 21:50:32 +0800

    fix Github Pages paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   
+  base: '/Cruzader-TKD/' // Set the base path for deployment
+  // If you are deploying to a custom domain, you might not need this line
+  // base: 'https://yourdomain.com/Cruzader-TKD/'
 })
